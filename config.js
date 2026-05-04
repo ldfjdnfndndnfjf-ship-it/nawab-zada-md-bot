5ce169bb7be12b85fa6ea1588f46e9b2dd7bfd04
Nawab512 <ldfjdnfndndnfjf@gmail.com>
2026-05-04 07:56:52 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    botName: "Nawab ZADA MD",
+    ownerName: "Nawab ZADA HACKER 🙌🦅",
+    ownerNumber: "+971528243405", // Apna sahi number yahan likhen
+    channelLink: "https://whatsapp.com/channel/0029VbB47ttDDmFNztpnZf2m",
+    prefix: "!",
+    forceJoin: true 
+};
+
+module.exports = config;
